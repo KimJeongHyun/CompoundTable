25b453e5d69353239ade40792aa99f12db644fc2
KimJeongHyun <ptr1129@paycrux.co.kr>
2023-10-19 20:47:38 +0900

    Fix : 패키지 세팅 오류 수정

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import commonjs from "@rollup/plugin-commonjs";
 import resolve from "@rollup/plugin-node-resolve";
 import typescript from "@rollup/plugin-typescript";
+import dts from "rollup-plugin-dts";
 
 const packageJson = require("./package.json");
 const external = ["react", "react-dom"];
@@ -29,5 +30,10 @@ export default [
       typescript({ tsconfig: "./tsconfig.json" }),
     ],
     external,
+  },
+  {
+    input: "src/index.ts",
+    output: [{ file: "dist/index.d.ts", format: "cjs" }],
+    plugins: [dts()],
   },
 ];
